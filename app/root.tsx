cfca76d9b5ea0c5be23a41b8a3bd64bf668f44f2
Ryan Killeen <rykilleen@gmail.com>
2022-02-13 22:38:31 -0500

    update page title

--- app/root.tsx
+++ app/root.tsx
@@ -9,7 +9,7 @@ import {
 import type { MetaFunction } from "remix";
 
 export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
+  return { title: "Ryan Killeen - Web Engineer" };
 };
 
 export default function App() {
