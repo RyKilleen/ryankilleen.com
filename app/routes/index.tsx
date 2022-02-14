fed48e6343e7ac42f35627e84c0ad4baa7169312
Ryan Killeen <rykilleen@gmail.com>
2022-02-13 22:36:18 -0500

    add resume, add content

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -16,53 +16,67 @@ export default function Index() {
                 alt="A selfie of Ryan standing in front of an orange, textured wall"
               />
             </div>
-            <p>
-              Ryan enjoys working on the web, learning, teaching, and tinkering.
-              Currently, he is a Senior Engineer at <a href="https://nerstreet.com">Nerd Street Gamers</a>
-            </p>
+            <div>
+              <p>
+                Ryan enjoys working on the web, learning, teaching, and tinkering.
+                Currently, he is a Senior Engineer at <a href="https://nerstreet.com">Nerd Street Gamers</a>
+              </p>
+              <p>Always a work in progress.</p>
+            </div>
           </div>
+          <hr />
         </section>
 
         <section>
           <h2>Lately, I've been</h2>
-
-          <div>
-            <h3>Working with:</h3>
-            <ul className="working">
-              <li><a href="https://nextjs.org">NextJS</a></li>
-              <li><a href="https://www.apollographql.com">Apollo GraphQL</a></li>
-              <li><a href="https://playwright.dev">Playwright</a></li>
-              <li><a href="https://stitches.dev">Stitches JS</a></li>
-            </ul>
-          </div>
-          <div>
-            <h3>Dabbling in:</h3>
-            <ul className="dabbling">
-              <li><a href="https://prisma.io">Prisma</a></li>
-              <li><a href="https://remix.run/">Remix.Run</a></li>
-              <li><a href="https://www.rust-lang.org/">Rust 🦀</a></li>
-              <li><a href="https://tauri.studio/">Tauri</a></li>
-              <li><a href="https://temporal.io">Temporal</a></li>
-            </ul>
-          </div>
-          <div>
-            <h3>Reading:</h3>
-            <ul className="reading">
-              <li><a href="https://www.amazon.com/Thinking-Systems-Donella-H-Meadows/dp/1603580557">Thinking in Systems: A Primer - <b>Donella H. Meadows</b></a></li>
-              <li><a href="https://staffeng.com/book">Staff Engineer: Leadership beyond the management track - <b>Will Larson</b></a></li>
-            </ul>
+          <div className="lately-holder">
+            <div>
+              <h3>Working with:</h3>
+              <ul className="working">
+                <li><a href="https://nextjs.org">NextJS</a></li>
+                <li><a href="https://www.apollographql.com">Apollo GraphQL</a></li>
+                <li><a href="https://sanity.io">Sanity CMS</a></li>
+                <li><a href="https://stitches.dev">Stitches JS</a></li>
+                <li><a href="https://playwright.dev">Playwright</a></li>
+                <li><a href="https://www.typescriptlang.org">TypeScript</a></li>
+              </ul>
+            </div>
+            <div>
+              <h3>Dabbling in:</h3>
+              <ul className="dabbling">
+                <li><a href="https://prisma.io">Prisma</a></li>
+                <li><a href="https://remix.run/">Remix.Run</a></li>
+                <li><a href="https://www.rust-lang.org/">Rust 🦀</a></li>
+                <li><a href="https://tauri.studio/">Tauri</a></li>
+                <li><a href="https://temporal.io">Temporal</a></li>
+                <li><a href="https://www.pulumi.com">Palumi</a></li>
+              </ul>
+            </div>
+            <div>
+              <h3>Reading:</h3>
+              <ul className="reading">
+                <li><a href="https://www.amazon.com/Thinking-Systems-Donella-H-Meadows/dp/1603580557">Thinking in Systems: A Primer - <b>Donella H. Meadows</b></a></li>
+                <li><a href="https://staffeng.com/book">Staff Engineer: Leadership beyond the management track - <b>Will Larson</b></a></li>
+              </ul>
+            </div>
           </div>
         </section>
       </main>
       <hr />
       <footer>
         <nav>
-          <ul style={{display: 'flex', listStyle: 'none', padding:0, gap: '1rem'}}>
+          <ul style={{ display: 'flex', listStyle: 'none', padding: 0, gap: '1rem' }}>
             <li>
               <a href="https://github.com/rykilleen">Github</a>
             </li>
             <li>
               <a href="https://twitter.com/deathbypapercut">Twitter</a>
+            </li>
+            <li>
+              <a href="mailto:rykilleen@gmail.com">Email</a>
+            </li>
+            <li>
+              <a href="/ryan-killeen-resume.pdf" target="_blank" rel="noopener noreferrer">Resume</a>
             </li>
           </ul>
         </nav>
