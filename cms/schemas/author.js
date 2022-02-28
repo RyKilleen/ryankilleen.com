93125af6e3c251e76bf0a93fa1df8fc56f3cec30
Ryan Killeen <rykilleen@gmail.com>
2022-02-28 17:07:24 -0500

    feat(cms): add interest related schema

--- cms/schemas/author.js
+++ cms/schemas/author.js
@@ -1,48 +1,49 @@
 export default {
-  name: 'author',
-  title: 'Author',
-  type: 'document',
+  name: "author",
+  title: "Author",
+  type: "document",
   fields: [
     {
-      name: 'name',
-      title: 'Name',
-      type: 'string',
+      name: "name",
+      title: "Name",
+      type: "string",
     },
     {
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
+      name: "slug",
+      title: "Slug",
+      type: "slug",
       options: {
-        source: 'name',
+        source: "name",
         maxLength: 96,
       },
     },
     {
-      name: 'image',
-      title: 'Image',
-      type: 'image',
+      name: "image",
+      title: "Image",
+      type: "image",
       options: {
         hotspot: true,
       },
     },
+    { name: "url", title: "Website", type: "url" },
     {
-      name: 'bio',
-      title: 'Bio',
-      type: 'array',
-      of: [
+      name: "twitter",
+      title: "Twitter",
+      type: "object",
+      fields: [
         {
-          title: 'Block',
-          type: 'block',
-          styles: [{title: 'Normal', value: 'normal'}],
-          lists: [],
+          name: "username",
+          title: "Username",
+          type: "string",
         },
+        { name: "url", title: "URL", type: "url" },
       ],
     },
   ],
   preview: {
     select: {
-      title: 'name',
-      media: 'image',
+      title: "name",
+      media: "image",
     },
   },
-}
+};
---
+++ cms/schemas/book.js
@@ -0,0 +1,51 @@
+export default {
+  name: "book",
+  title: "Book",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "author",
+      title: "Author",
+      type: "reference",
+      to: {
+        type: "author",
+      },
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+    },
+    {
+      name: "featured",
+      title: "Featured",
+      type: "boolean",
+      initialValue: false,
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "text",
+    },
+    {
+      name: "url",
+      title: "URL",
+      type: "url",
+    },
+    {
+      name: "categories",
+      title: "Categories",
+      type: "array",
+      of: [{ type: "reference", to: { type: "category" } }],
+    },
+  ],
+};
