b3eff9b49eca248eefea04dcf50fd43129123e34
Ryan Killeen <ryan@ryankilleen.com>
2025-03-04 17:49:58 -0500

    chore: update color scheme

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 export default {
-	title: "Eleventy Base Blog v9",
-	url: "https://example.com/",
-	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
-	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
-	}
-}
+  title: "Ryan Killeen - Web Developer, Among Other Things",
+  url: "https://ryankilleen.com/",
+  language: "en",
+  description: "",
+  author: {
+    name: "Ryan Killeen",
+    email: "web@ryankilleen.com",
+    url: "https://ryankilleen.com/about",
+  },
+};
